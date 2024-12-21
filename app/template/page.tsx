13210e20530d6239ec93769a6156ff91d6cb4dec
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-12-22 00:02:01 +0200

    Add Dropbox in Templates page

--- app/template/page.tsx
+++ app/template/page.tsx
@@ -5,8 +5,50 @@ import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessa
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import PublicLayout from "../layouts/public";
+import * as React from "react"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import { Check, ChevronsUpDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+]
 
 export default function TemplatePage() {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
   return (
     <PublicLayout title="Templates">
       <div>
@@ -23,8 +65,55 @@ export default function TemplatePage() {
           <div className="flex items-center justify-center">
 
           <div className="flex w-full max-w-sm items-center space-x-2">
-            <Input placeholder="Search..." />
-            <Button type="submit">Search</Button>
+            <div className="flex flex-direction-col space-x-2">
+              <div className="flex w-full max-w-sm items-center space-x-2">
+                <Input placeholder="Search..." />
+                <Button type="submit">Search</Button>
+              </div>
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant="outline"
+                    role="combobox"
+                    aria-expanded={open}
+                    className="w-[200px] justify-between"
+                  >
+                    {value
+                      ? frameworks.find((framework) => framework.value === value)?.label
+                      : "Select category..."}
+                    <ChevronsUpDown className="opacity-50" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-[200px] p-0">
+                  <Command>
+                    <CommandInput placeholder="Search category..." className="h-9" />
+                    <CommandList>
+                      <CommandEmpty>No framework found.</CommandEmpty>
+                      <CommandGroup>
+                        {frameworks.map((framework) => (
+                          <CommandItem
+                            key={framework.value}
+                            value={framework.value}
+                            onSelect={(currentValue) => {
+                              setValue(currentValue === value ? "" : currentValue)
+                              setOpen(false)
+                            }}
+                          >
+                            {framework.label}
+                            <Check
+                              className={cn(
+                                "ml-auto",
+                                value === framework.value ? "opacity-100" : "opacity-0"
+                              )}
+                            />
+                          </CommandItem>
+                        ))}
+                      </CommandGroup>
+                    </CommandList>
+                  </Command>
+                </PopoverContent>
+              </Popover>  
+            </div>
           </div>
 
           </div>
