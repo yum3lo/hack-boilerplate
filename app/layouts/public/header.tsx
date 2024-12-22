87a8540f9803269325f89ef817bb99c055613e5a
maria <118808220+yum3lo@users.noreply.github.com>
2024-12-22 12:52:37 +0200

    auth

--- app/layouts/public/header.tsx
+++ app/layouts/public/header.tsx
@@ -77,8 +77,8 @@ export function Header() {
               <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="font-normal">
                   <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">{user?.name}</p>
-                    <p className="text-xs leading-none text-muted-foreground">{user?.email}</p>
+                    <p className="text-sm font-medium leading-none">{"Maria"}</p>
+                    <p className="text-xs leading-none text-muted-foreground">{"maria@email.com"}</p>
                   </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
