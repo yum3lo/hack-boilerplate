d16e8d6eca8702eaeef5a8880a20ade87e7b568e
maria <118808220+yum3lo@users.noreply.github.com>
2024-12-22 03:25:40 +0200

    Update header.tsx

--- app/layouts/public/header.tsx
+++ app/layouts/public/header.tsx
@@ -30,17 +30,19 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 export function Header() {
   const router = useRouter();
-  const { isAuthenticated, logout } = useAuth();
+  // const { isAuthenticated, logout } = useAuth();
+  const isAuthenticated = true;
+  const logout = false;
 
-  const handleLogout = async () => {
-    try {
-      await logout();
-      // Redirect to home page after logout
-      router.push('/');
-    } catch (error) {
-      console.error('Logout failed:', error);
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     await logout();
+  //     // Redirect to home page after logout
+  //     router.push('/');
+  //   } catch (error) {
+  //     console.error('Logout failed:', error);
+  //   }
+  // };
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
@@ -56,12 +58,12 @@ export function Header() {
             <SquareLibrary className="h-5 w-5"/>
             <Link href="/glossary">Glossary</Link>
           </Button>
-          {/* {isAuthenticated && ( */}
+          {isAuthenticated && (
             <Button variant="ghost"> 
               <Folder className="h-5 w-5"/>
               <Link href="/docs">My docs</Link>
             </Button>
-          {/* )} */}
+          )}
           <Button variant="ghost">
             <LayoutTemplate className="h-5 w-5"/>
             <Link href="/template">Templates</Link>
@@ -74,7 +76,7 @@ export function Header() {
                 <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                   <Avatar className="h-8 w-8">
                     <AvatarImage src={"/avatar.png"} alt={"Avatar"} />
-                    <AvatarFallback>{"example@email.com"}</AvatarFallback>
+                    <AvatarFallback>{""}</AvatarFallback>
                   </Avatar>
                 </Button>
               </DropdownMenuTrigger>
@@ -91,7 +93,9 @@ export function Header() {
                     <span>Account</span>
                   </Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem onClick={handleLogout} className="text-red-600 focus:text-red-600">
+                <DropdownMenuItem 
+                  // onClick={handleLogout} 
+                  className="text-red-600 focus:text-red-600">
                   <LogOut className="mr-2 h-4 w-4" />
                   <span>Log out</span>
                 </DropdownMenuItem>
